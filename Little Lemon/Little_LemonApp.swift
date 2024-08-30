51822cd2d58fd98365fe5f315434f0d34b47f987
Jon Iger <jon.iger@outlook.com>
2024-08-30 05:23:27 -0700

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Jon Iger on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
